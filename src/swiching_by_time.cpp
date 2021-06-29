365c6bf96e7014511abc397d244485fa48854163
haruyama8940 <yaruo0406@gmail.com>
2021-06-30 03:52:11 +0900

    debug

--- src/swiching_by_time.cpp
+++ src/swiching_by_time.cpp
@@ -2,22 +2,23 @@
 #include <std_srvs/SetBool.h>                // サービスヘッダファイル
 #include <std_srvs/Trigger.h>                // サービスヘッダファイル
 #include <nav_msgs/Odometry.h>
-#include "geometry_msgs/PoseWithCovarianceStamped.h"
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 
 //servise callback
 //flag == true
 bool ch_flag =false;
 //float pose_x,pose_y,pose_z,ori_x,ori_y,ori_z,ori_w;
 double wait_t;
-ros::NodeHandle nh;                    // ノードハンドルの生成
-ros::ServiceClient Start_Wp_Client = nh.serviceClient<std_srvs::Trigger>("start_wp_nav");  // クライアントの生成
-ros::ServiceClient StartClient = nh.serviceClient<std_srvs::SetBool>("start_learning");  // クライアントの生成
+
 std_srvs::Trigger::Request req;             // リクエストの生成
 std_srvs::Trigger::Response resp;           // レスポンスの生成
 std_srvs::SetBool::Request req_ler;             // リクエストの生成
 std_srvs::SetBool::Response resp_ler;           // レスポンスの生成
 geometry_msgs::PoseWithCovarianceStamped pose_msg;
-ros::Publisher initial_pose_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 10);
+ros::ServiceClient Start_Wp_Client; 
+ros::ServiceClient StartClient; 
+ros::ServiceServer srv;
+ros::Publisher initial_pose_pub; 
 
 void pose_set(float pose_x,float pose_y,float ori_z,float ori_w)
     {
@@ -33,8 +34,20 @@ void pose_set(float pose_x,float pose_y,float ori_z,float ori_w)
     pose_msg.pose.pose.orientation.z = ori_z;
     pose_msg.pose.pose.orientation.w = ori_w;
  
+    } 
+
+void mode(double t)
+    {
+        req_ler.data=true;
+        bool  start_learning = StartClient.call(req_ler,resp_ler); //学習の開始
+        ROS_INFO("learning_mode!");
+        ros::Duration(t).sleep();
+        req_ler.data=false;
+        start_learning = StartClient.call(req_ler,resp_ler); //学習の終了
+        initial_pose_pub.publish(pose_msg);
+        bool  start_waypointnav = Start_Wp_Client.call(req,resp); //way restart
+        ROS_INFO("waypoint_mode!");
     }
-//callbackの前にreq.dataで分ける
 bool serviceCallBack(std_srvs::SetBool::Request &req_ler,
 std_srvs::SetBool::Response &resp_ler)
  {
@@ -47,24 +60,18 @@ std_srvs::SetBool::Response &resp_ler)
             wait_t = 10.0;
             pose_set(0,0,0,0);
         }
- } 
-
-void mode(double t)
-    {
-        bool  start_learning = StartClient.call(req_ler,resp_ler); //学習の開始
-        ROS_INFO("learning_mode!");
-        ros::Duration(t).sleep();
-        start_learning = StartClient.call(req_ler,resp_ler); //学習の終了
-        initial_pose_pub.publish(pose_msg);
-        bool  start_waypointnav = Start_Wp_Client.call(req,resp); //way restart
-        ROS_INFO("waypoint_mode!");
-    }
+        mode(wait_t);
+ }
 
 /* サーバー */
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "swiching_by_time");        // ノードの初期化（ノード名を"srv_test"に設定）
-    ros::ServiceServer srv = nh.advertiseService("start_call", &serviceCallBack);  // サーバー生成とコールバック関数の登録
-    mode(wait_t);
+    ros::init(argc, argv, "swiching_by_time");        // ノードの初期化（ノード名を"srv_test"に設定
+    ros::NodeHandle nh;                    // ノードハンドルの生成
+    initial_pose_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 10);
+    Start_Wp_Client = nh.serviceClient<std_srvs::Trigger>("start_wp_nav");  // クライアントの生成
+    StartClient = nh.serviceClient<std_srvs::SetBool>("learn_out");  // クライアントの生成
+    ROS_INFO("ready!");
+    srv = nh.advertiseService("start_call", &serviceCallBack);  // サーバー生成とコールバック関数の登録
     ros::spin();                              // リクエストの無限待ち
   return 0;
 }   
